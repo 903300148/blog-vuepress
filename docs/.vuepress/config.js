c002b41ce9e50416f4c9dc31d27c874de8f0e0c5
cds <903300148@qq.com>
2020-03-17 01:23:01 +0800

    'deploy'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,111 @@
+module.exports = {
+    title: '小怂博客',
+    description: '开发者的博客世界',
+    head: [
+        [
+            'link',
+            {
+                rel: 'icon',
+                href: `/favicon.ico`
+            }
+        ]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '个人简介', link: '/own/' },
+            { text: '文笔生花', link: '/article/' },
+            { text: '前沿技术', link: '/ft/' },
+            {
+                text: '语言',
+                items: [
+                    { text: '中文', link: '/language/chinese/' },
+                    { text: 'English', link: '/language/english/' }
+                ]
+            },
+            { text: 'External', link: 'https://www.baidu.com' }
+        ],
+        sidebarDepth: 2,
+        sidebar: {
+            // 侧边栏在 /index/ 目录上
+            '/own/': [
+                ['', '个人简介'],
+                ['/own/个人技术', '个人技术'],
+                ['/own/个人爱好', '个人爱好']
+            ],
+            '/article/': [
+                ['', '文笔生花'],
+                {
+                    title: 'Vue',
+                    children: [
+                        ['/article/vue篇/基础篇', '基础篇'],
+                        ['/article/vue篇/进阶篇', '进阶篇'],
+                        ['/article/vue篇/源码解读篇', '源码解读篇'],
+                        ['/article/vue篇/业务篇', '业务篇']
+                    ]
+                },
+                {
+                    title: 'React',
+                    children: [
+                        ['/article/react篇/基础篇', '基础篇'],
+                        ['/article/react篇/进阶篇', '进阶篇'],
+                        ['/article/react篇/源码解读篇', '源码解读篇'],
+                        ['/article/react篇/业务篇', '业务篇']
+                    ]
+                },
+                {
+                    title: '小程序',
+                    children: [
+                        ['/article/小程序篇/基础篇', '基础篇'],
+                        ['/article/小程序篇/进阶篇', '进阶篇'],
+                        ['/article/小程序篇/源码解读篇', '源码解读篇'],
+                        ['/article/小程序篇/业务篇', '业务篇']
+                    ]
+                },
+                {
+                    title: 'Webpack',
+                    children: [
+                        ['/article/webpack篇/基础篇', '基础篇'],
+                        ['/article/webpack篇/进阶篇', '进阶篇'],
+                        ['/article/webpack篇/源码解读篇', '源码解读篇'],
+                        ['/article/webpack篇/业务篇', '业务篇']
+                    ]
+                }
+            ],
+            '/ft/': [
+                ['', '前沿技术'],
+                {
+                    title: 'GraphQL',
+                    children: [
+                        ['/ft/GraphQL篇/初识', '初识'],
+                        ['/ft/GraphQL篇/应用', '应用']
+                    ]
+                },
+                {
+                    title: 'serverless',
+                    children: [
+                        ['/ft/serverless篇/初识', '初识'],
+                        ['/ft/serverless篇/应用', '应用']
+                    ]
+                },
+                {
+                    title: 'WebAssembly',
+                    children: [
+                        ['/ft/WebAssembly篇/初识', '初识'],
+                        ['/ft/WebAssembly篇/应用', '应用']
+                    ]
+                },
+                {
+                    title: '前端微服务',
+                    children: [
+                        ['/ft/前端微服务篇/初识', '初识'],
+                        ['/ft/前端微服务篇/应用', '应用']
+                    ]
+                }
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /favicon.ico
+actionText: 点击进入 →
+actionLink: /home/
+features:
+- title: 个人简介
+  details: 作者的前端生涯。
+- title: 文笔生花
+  details: 希望每一篇技术博文能带给您一丝丝感悟便是成功。
+- title: 前沿技术
+  details: 关注互联网时代最新潮的技术,享受互联网带来的无限神秘。
+footer: MIT Licensed | Copyright © 2019-present
+---
